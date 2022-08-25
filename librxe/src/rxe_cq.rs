8460150b86618a848736dcb3698c352d668c52b0
zelin <wangzelin20s@ict.ac.cn>
2022-08-25 10:26:56 +0800

    feat(rxe-cq): impl post a cqe to the cq

--- librxe/src/rxe_cq.rs
+++ librxe/src/rxe_cq.rs
@@ -1,11 +1,28 @@
 use std::ptr::NonNull;
 
 use derivative::Derivative;
-use librxe_sys::rxe_cq;
+use librxe_sys::{advance_producer, producer_addr, queue_full, rxe_cq};
 use nix::Error;
 use rdma_sys::{ibv_cq, ibv_create_cq, ibv_poll_cq};
 
 use crate::rxe_context::RxeContext;
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct RxeCqe {
+    pub wc: rdma_sys::ibv_wc,
+}
+
+impl Default for RxeCqe {
+    // like malloc and memset to 0
+    fn default() -> Self {
+        let mut s = ::std::mem::MaybeUninit::<Self>::uninit();
+        unsafe {
+            ::std::ptr::write_bytes(s.as_mut_ptr(), 0, 1);
+            s.assume_init()
+        }
+    }
+}
 
 /// Complete Queue Structure
 #[derive(Debug, Clone, Derivative)]
@@ -68,26 +85,55 @@ impl RxeCompletionQueue {
     pub fn poll<'c>(
         &self,
         completions: &'c mut [rdma_sys::ibv_wc],
+        userspace: bool,
     ) -> Result<&'c mut [rdma_sys::ibv_wc], ()> {
-        // TODO: from http://www.rdmamojo.com/2013/02/15/ibv_poll_cq/
-        //
-        //   One should consume Work Completions at a rate that prevents the CQ from being overrun
-        //   (hold more Work Completions than the CQ size). In case of an CQ overrun, the async
-        //   event `IBV_EVENT_CQ_ERR` will be triggered, and the CQ cannot be used anymore.
-        //
-        let n = unsafe {
-            ibv_poll_cq(
+        let n = if !userspace {
+            // just for debug
+            // TODO: from http://www.rdmamojo.com/2013/02/15/ibv_poll_cq/
+            //
+            //   One should consume Work Completions at a rate that prevents the CQ from being overrun
+            //   (hold more Work Completions than the CQ size). In case of an CQ overrun, the async
+            //   event `IBV_EVENT_CQ_ERR` will be triggered, and the CQ cannot be used anymore.
+            //
+            unsafe {
+                ibv_poll_cq(
+                    self.as_ptr(),
+                    completions.len() as i32,
+                    completions.as_mut_ptr(),
+                )
+            }
+        } else {
+            crate::rxe_poll_cq(
                 self.as_ptr(),
                 completions.len() as i32,
                 completions.as_mut_ptr(),
             )
         };
-
         if n < 0 {
             Err(())
         } else {
             Ok(&mut completions[0..n as usize])
         }
+    }
+
+    #[inline]
+    pub unsafe fn cq_post(
+        cq: &mut NonNull<rxe_cq>,
+        cqe: &mut RxeCqe,
+        _solicited: bool,
+    ) -> Result<(), Error> {
+        libc::pthread_spin_lock(&mut cq.as_mut().lock);
+        if queue_full(cq.as_ref().queue) {
+            libc::pthread_spin_unlock(&mut cq.as_mut().lock);
+            return Err(Error::EBUSY);
+        }
+        let dst_addr: *mut rdma_sys::ibv_wc = producer_addr(cq.as_mut().queue);
+        std::ptr::copy_nonoverlapping(&cqe.wc as *const rdma_sys::ibv_wc, dst_addr, 1);
+        advance_producer(cq.as_mut().queue);
+
+        libc::pthread_spin_unlock(&mut cq.as_mut().lock);
+        // should call comp_task here when solicited is true
+        Ok(())
     }
 }
 
